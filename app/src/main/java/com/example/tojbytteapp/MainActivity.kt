fed938729f268160e4fb7d700ce45638038d4248
Simon <junkersje@gmail.com>
2025-03-03 15:43:02 +0100

    Cleaned committed files

--- app/src/main/java/com/example/tojbytteapp/MainActivity.kt
+++ app/src/main/java/com/example/tojbytteapp/MainActivity.kt
@@ -23,16 +23,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.foundation.border
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.Dp
 
 class MainActivity : ComponentActivity() {
